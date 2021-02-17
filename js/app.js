7065104eeabdfd08c9a881fb201326b73bc07906
Santiago <58869214+Santiagovillalobos-12@users.noreply.github.com>
2021-02-17 18:19:01 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,28 +2,22 @@ function toggleMenu() {
     try {
         const toggle = document.querySelector('.toggle');
         const navigation = document.querySelector('.navigation-movil');
-        const links = document.querySelectorAll('.navigation li a');
         const linksMovil = document.querySelectorAll('.navigation-movil li a');
-
-
 
         function active() {
             toggle.classList.toggle('active');
             navigation.classList.toggle('active');
         }
         toggle.addEventListener('click', active);
-        links.forEach(i => {
-            i.addEventListener('click', active);
-        })
         linksMovil.forEach(i => {
             i.addEventListener('click', active);
         })
-
 
     } catch (error) {
         console.log(`error`, error);
     }
 }
+
 /* 
 function containerBx() {
 
@@ -42,19 +36,23 @@ function containerBx() {
 
 function positionToggleResponsive() {
     try {
-        
-            window.addEventListener('scroll', function(){
-                const toggle = document.querySelector('.toggle');
-                toggle.setAttribute('style', 'display: initial', window.scrollY >= 0);
-                if(window.scrollY ==0){
-                    toggle.setAttribute('style','display: none');
-                }
-            })
-        }
+        window.addEventListener('scroll', function () {
+            const toggle = document.querySelector('.toggle');
+            toggle.setAttribute('style', 'display: initial', window.scrollY >= 0);
+            if (window.scrollY == 0){
+                toggle.setAttribute('style', 'display: none');
+            }
+            if(screen.width > 767){
+                toggle.setAttribute('style', 'display: none');
+            }
+        })
+    }
     catch (error) {
         console.log(`error`, error);
     }
 }
+
+
 toggleMenu();
 /* containerBx(); */
 positionToggleResponsive();
